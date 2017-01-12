2110d2b3e94472cce787b329fa7cd787b1fd89af
Lucas Alcântara <lucashmalcantara@gmail.com>
2017-01-11 23:10:52 -0200

    Algumas mudanças.

--- app/src/main/java/com/lucasalcantara/controledeponto/view/DatePickerFragment.java
+++ app/src/main/java/com/lucasalcantara/controledeponto/view/DatePickerFragment.java
@@ -2,32 +2,31 @@ package com.lucasalcantara.controledeponto.view;
 
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
-import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 /**
  * Created by Lucas on 05/01/2017.
  */
-public class TimePickerFragment extends DialogFragment {
+public class DatePickerFragment extends DialogFragment {
 
     private Activity mActivity;
-    private TimePickerDialog.OnTimeSetListener mListener;
+    private DatePickerDialog.OnDateSetListener mListener;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mActivity = activity;
 
-        // Este erro lembrará de implementar OnTimeSetListener
+        // Este erro lembrará de implementar OnDateSetListener
         // na sua Activity caso esqueça.
         try {
-            mListener = (TimePickerDialog.OnTimeSetListener) activity;
+            mListener = (DatePickerDialog.OnDateSetListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement OnTimeSetListener");
         }
@@ -35,13 +34,11 @@ public class TimePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Pega a data atual.
         final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Cria uma instância do TimePickerDialog e retorna.
-        return new TimePickerDialog(mActivity, mListener, hour, minute,
-                DateFormat.is24HourFormat(mActivity));
+        return new DatePickerDialog(mActivity,mListener, year, month, day);
     }
 }
--- app/src/main/java/com/lucasalcantara/controledeponto/view/EditarEntradaActivity.java
+++ app/src/main/java/com/lucasalcantara/controledeponto/view/EditarEntradaActivity.java
@@ -1,19 +1,25 @@
 package com.lucasalcantara.controledeponto.view;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.lucasalcantara.controledeponto.R;
 
 import java.text.DecimalFormat;
+import java.util.Calendar;
 
-public class EditarEntradaActivity extends AppCompatActivity  implements TimePickerDialog.OnTimeSetListener {
+public class EditarEntradaActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
     TextView horaTextView = null;
+    TextView dataTextView = null;
+    Button feitoButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +33,65 @@ public class EditarEntradaActivity extends AppCompatActivity  implements TimePic
                 mostrarTimePickerDialog(v);
             }
         });
+
+        dataTextView = (TextView) findViewById(R.id.dataTextView);
+        dataTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDatePickerDialog(v);
+            }
+        });
+
+        final Calendar calendar = Calendar.getInstance();
+        preencherTextViewData(calendar.get(Calendar.DAY_OF_MONTH), (calendar.get(Calendar.MONTH) + 1), calendar.get(Calendar.YEAR));
+        preencherTextViewHora(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+
+        feitoButton = (Button) findViewById(R.id.feitoButton);
+        feitoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    //region Referente aos dialogs
 
     public void mostrarTimePickerDialog(View v) {
         DialogFragment fragmento = new TimePickerFragment();
         fragmento.show(getSupportFragmentManager(), "timePicker");
     }
 
+    public void mostrarDatePickerDialog(View v) {
+        DialogFragment fragmento = new DatePickerFragment();
+        fragmento.show(getSupportFragmentManager(), "datePicker");
+    }
+
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        preencherTextViewHora(hourOfDay, minute);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        preencherTextViewData((dayOfMonth + 1), monthOfYear, year);
+    }
+    //endregion
+
+    private void preencherTextViewData(int dia, int mes, int ano) {
         DecimalFormat df = new DecimalFormat("00");
-        String hora = String.valueOf(df.format(hourOfDay)) + ":" + String.valueOf(df.format(minute));
-        horaTextView.setText(hora);
+        String data = String.valueOf(String.format("%1$s/%2$s/%3$s",
+                df.format(dia), df.format(mes), ano));
+        dataTextView.setText(data);
+    }
+
+    private void preencherTextViewHora(int hora, int minuto) {
+        DecimalFormat df = new DecimalFormat("00");
+        String horario = String.valueOf(df.format(hora)) + ":" + String.valueOf(df.format(minuto));
+        horaTextView.setText(horario);
+    }
+
+    private void salvarEntrada(){
+
     }
 }
